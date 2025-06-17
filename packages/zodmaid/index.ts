68988c4f779d78760b8deb1db8de922d4792c3bd
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-17 10:32:27 +0200

    chore: Package build exports.

--- packages/zodmaid/index.ts
+++ packages/zodmaid/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello via Bun!");
+export {
+  arrow,
+  attr,
+  cell,
+  edge,
+  id,
+  label,
+  node,
+  shape,
+  type AttrType,
+  type CellType,
+  type DiagramType,
+  type EdgeType,
+  type LabelType,
+  type NodeType,
+} from "./domains/diagramDomain";
+export {
+  layout,
+  populate,
+  render,
+  textBbox,
+  type DaGraph,
+  type DiagramOptions,
+} from "./engines/dagreEngine";
