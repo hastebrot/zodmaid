cebae199b0545e765ba848a210ddce2fac1d5e0d
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-25 17:11:51 +0200

    wip

--- packages/zodmaid-app/src/routes/datagridPage.tsx
+++ packages/zodmaid-app/src/routes/datagridPage.tsx
@@ -1,39 +1,157 @@
-import "react-data-grid/lib/styles.css";
-
 import { useEffect, useRef, useState, type Key } from "react";
-import { Cell, DataGrid, Row, type Column, type DataGridHandle } from "react-data-grid";
+import { Cell, DataGrid, Row, textEditor, type Column, type DataGridHandle } from "react-data-grid";
+import "react-data-grid/lib/styles.css";
+import { classNames } from "../helpers/clsx";
 
 interface Row {
-  id: number;
-  title: string;
+  index: number;
+  businessCase: string;
+  objectKey: string;
+  amount: number;
+  currency: string;
+  bookingDate: string;
+  paymentDate: string;
+  debtorId: string;
+  creditorId: string;
+  documentId: string;
+  parentId: string;
+  userId: string;
+  sourceSystem: string;
+  tariffCode: string;
+  agentId: string;
+  provisionAmount: number;
+  agioAmount: number;
 }
 
 const columns: Column<Row>[] = [
   {
-    key: "id",
-    name: "ID",
+    key: "index",
+    name: "Index",
     width: "max-content",
   },
   {
-    key: "title",
-    name: "Title",
+    key: "businessCase",
+    name: "Business Case",
     renderHeaderCell(props) {
-      return <div className="_bg-red-500">{props.column.name}</div>;
+      return <div>{props.column.name}</div>;
     },
     renderCell(props) {
-      return <div>{props.row.title}</div>;
+      return (
+        <div className="flex gap-1 items-stretch">
+          <button
+            className="hover:outline cursor-pointer px-0.5 flex items-center"
+            type="button"
+            onClick={() => {
+              console.log("button");
+            }}
+          >
+            <div className="text-blue-500 font-mono text-sm/none">{`""`}</div>
+          </button>
+          <div className="flex gap-1 items-center">
+            <div className="text-green-500 font-mono text-sm/none">{`//`}</div>
+            <div className="text-red-500 font-mono text-sm/none">{`<>`}</div>
+            <div className="text-red-500 font-mono text-sm/none">{`{}`}</div>
+            <div className="text-red-500 font-mono text-sm/none">{`[]`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`""`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`#`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`01`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`Ø`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`$`}</div>
+            {props.row.businessCase}
+          </div>
+        </div>
+      );
     },
+    renderEditCell: textEditor,
     editable: true,
+  },
+  {
+    key: "objectKey",
+    name: "Object Key",
+  },
+  {
+    key: "amount",
+    name: "Amount",
+  },
+  {
+    key: "currency",
+    name: "Currency",
+  },
+  {
+    key: "bookingDate",
+    name: "Booking Date",
+  },
+  {
+    key: "paymentDate",
+    name: "Payment Date",
+  },
+  {
+    key: "debtorId",
+    name: "Debtor ID",
+  },
+  {
+    key: "creditorId",
+    name: "Creditor ID",
+  },
+  {
+    key: "documentId",
+    name: "Document ID",
+  },
+  {
+    key: "parentId",
+    name: "Parent ID",
+  },
+  {
+    key: "userId",
+    name: "User ID",
+  },
+  {
+    key: "sourceSystem",
+    name: "Source System",
+  },
+  {
+    key: "tariffCode",
+    name: "Tariff Code",
+  },
+  {
+    key: "agentId",
+    name: "Agent ID",
+  },
+  {
+    key: "provisionAmount",
+    name: "Provision Amount",
+  },
+  {
+    key: "agioAmount",
+    name: "Agio Amount",
   },
 ];
 
-const rows: Row[] = [
-  { id: 0, title: "Example" },
-  { id: 1, title: "Demo" },
+const _rows: Row[] = [
+  {
+    index: 1,
+    businessCase: "contract_payment",
+    objectKey: "CONTAINER_123456",
+    amount: 1250.0,
+    currency: "EUR",
+    bookingDate: "2025-06-24",
+    paymentDate: "2025-06-24",
+    debtorId: "CRM_USER_00123",
+    creditorId: "ASSET_MGR_00987",
+    documentId: "INV_2025_0633",
+    parentId: "CHAIN_473928",
+    userId: "SYS_FINANCEBOT",
+    sourceSystem: "CRM",
+    tariffCode: "T-STD-24A",
+    agentId: "AGT_333",
+    provisionAmount: 50.0,
+    agioAmount: 10.0,
+  },
 ];
 
 export const DatagridPage = () => {
   const ref = useRef<DataGridHandle>(null);
+  const [rows, setRows] = useState<Row[]>(_rows);
   const [selectedRows, setSelectedRows] = useState<Set<Key>>();
   useEffect(() => {
     console.log("selected row change");
@@ -58,8 +176,9 @@ export const DatagridPage = () => {
       // ref.current.selectCell({ idx: 0, rowIdx: 0 }, { shouldFocusCell: true, enableEditor: false });
     }
   };
+
   return (
-    <div className="min-h-dvh bg-[#212121] p-4" onClick={onClick}>
+    <div className="min-h-dvh bg-[#212121] p-4">
       <DataGrid
         selectedRows={selectedRows}
         onSelectedRowsChange={setSelectedRows}
@@ -67,7 +186,14 @@ export const DatagridPage = () => {
         ref={ref}
         columns={columns}
         rows={rows}
+        onRowsChange={setRows}
         // headerRowHeight={0}
+        defaultColumnOptions={{
+          resizable: true,
+        }}
+        headerRowClass={classNames(
+          "[&>div]:border-t [&>div]:border-[#444444] [&>div]:first:border-l",
+        )}
         renderers={{
           renderCell(key, props) {
             return <Cell key={key} {...props} className="border-[#444444] first:border-l" />;
@@ -80,17 +206,19 @@ export const DatagridPage = () => {
           console.log("selected cell change", event);
         }}
         onCellMouseDown={(props, event) => {
-          // event.preventGridDefault();
+          if (event.target instanceof HTMLElement && event.target.tagName === "BUTTON") {
+            event.preventGridDefault();
+          }
           // event.stopPropagation();
         }}
         onCellClick={(props, event) => {
-          // event.preventGridDefault();
+          event.preventGridDefault();
           // console.log("cell click");
           // event.stopPropagation();
           // props.selectCell(true);
         }}
         onCellDoubleClick={(props, event) => {
-          // event.preventGridDefault();
+          event.preventGridDefault();
         }}
       />
     </div>
