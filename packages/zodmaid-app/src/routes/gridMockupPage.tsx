eddc91551b31475efea7f56f1ef76fb229266537
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-25 22:51:50 +0200

    (wip)

--- packages/zodmaid-app/src/routes/gridMockupPage.tsx
+++ packages/zodmaid-app/src/routes/gridMockupPage.tsx
@@ -71,33 +71,6 @@ export const GridMockupPage = () => {
       <Row>
         <Cell>
           <div className="flex gap-1 items-start">
-            <GridExpand />
-            <div className="flex gap-1 items-center">
-              <JsonType variant="composite">{`[]`}</JsonType>
-              <JsonKey>items</JsonKey>
-            </div>
-          </div>
-        </Cell>
-        <Cell>
-          <div className="flex gap-1 items-start">
-            <GridExpand />
-            <div className="flex gap-1 items-center">
-              <JsonType variant="composite">{`{}`}</JsonType>
-              <JsonIndex>5</JsonIndex>
-            </div>
-          </div>
-        </Cell>
-        <Cell>
-          <div className="flex gap-1 items-start">
-            <GridExpand />
-            <div className="flex gap-1 items-center">
-              <JsonType variant="composite">{`<>`}</JsonType>
-              <JsonIndex>15</JsonIndex>
-            </div>
-          </div>
-        </Cell>
-        <Cell>
-          <div className="flex gap-1 items-start">
             <div className="flex gap-1 items-center">
               <JsonType variant="primitive">{`"A"`}</JsonType>
               <JsonValue>123-456-7890</JsonValue>
@@ -145,6 +118,47 @@ export const GridMockupPage = () => {
           </div>
         </Cell>
       </Row>
+      <Row>
+        <Cell>
+          <div className="flex gap-1 items-start">
+            <GridExpand />
+            <div className="flex flex-col gap-1">
+              <div className="flex gap-1 items-center">
+                <JsonType variant="composite">{`[]`}</JsonType>
+                <JsonKey>items</JsonKey>
+              </div>
+              <div className="inline-block ml-[-1px]">
+                <div className="[&>svg>g]:fill-none scale-[calc(18/24)] origin-top-left text-zinc-700 opacity-40">
+                  {iconTable}
+                </div>
+              </div>
+              <div className="inline-block ml-[-1px]">
+                <div className="scale-[calc(18/24)] origin-top-left text-zinc-700 opacity-100">
+                  {iconTable}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Cell>
+        <Cell>
+          <div className="flex gap-1 items-start">
+            <GridExpand />
+            <div className="flex gap-1 items-center">
+              <JsonType variant="composite">{`{}`}</JsonType>
+              <JsonIndex>5</JsonIndex>
+            </div>
+          </div>
+        </Cell>
+        <Cell>
+          <div className="flex gap-1 items-start">
+            <GridExpand />
+            <div className="flex gap-1 items-center">
+              <JsonType variant="composite">{`<>`}</JsonType>
+              <JsonIndex>15</JsonIndex>
+            </div>
+          </div>
+        </Cell>
+      </Row>
     </div>
   );
 };
@@ -158,7 +172,7 @@ const GridExpand = () => {
         "outline-gray-300 outline outline-offset-0",
       )}
     >
-      <div className="inline-block scale-60 origin-top-left">{caretUp}</div>
+      <div className="inline-block scale-60 origin-top-left">{iconCaretUp}</div>
     </div>
   );
 };
@@ -168,7 +182,7 @@ const JsonComment = (props: { children?: React.ReactNode }) => {
 };
 
 const JsonIndex = (props: { children?: React.ReactNode }) => {
-  return <div className="font-[700] text-sm/none text-zinc-400">{props.children}</div>;
+  return <div className="font-[700] text-sm/none opacity-40">{props.children}</div>;
 };
 
 const JsonValue = (props: { children?: React.ReactNode }) => {
@@ -224,7 +238,7 @@ const Cell = (props: { children?: React.ReactNode; variant?: "yellow" | "green" 
   );
 };
 
-const caretUp = (
+const iconCaretUp = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
@@ -235,5 +249,30 @@ const caretUp = (
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M11.293 7.293a1 1 0 0 1 1.32 -.083l.094 .083l6 6l.083 .094l.054 .077l.054 .096l.017 .036l.027 .067l.032 .108l.01 .053l.01 .06l.004 .057l.002 .059l-.002 .059l-.005 .058l-.009 .06l-.01 .052l-.032 .108l-.027 .067l-.07 .132l-.065 .09l-.073 .081l-.094 .083l-.077 .054l-.096 .054l-.036 .017l-.067 .027l-.108 .032l-.053 .01l-.06 .01l-.057 .004l-.059 .002h-12c-.852 0 -1.297 -.986 -.783 -1.623l.076 -.084l6 -6z" />
+  </svg>
+);
+
+const iconTable = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    className="icon icon-tabler icons-tabler-outline icon-tabler-table"
+  >
+    <g className="fill-yellow-300" stroke="none">
+      <path d="M4 11h4a1 1 0 0 1 1 1v8a1 1 0 0 1 -1 1h-2a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-6a1 1 0 0 1 1 -1z" />
+      <path d="M18 3a3 3 0 0 1 2.995 2.824l.005 .176v2a1 1 0 0 1 -1 1h-8a1 1 0 0 1 -1 -1v-4a1 1 0 0 1 1 -1h6z" />
+      <path d="M9 4v4a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a3 3 0 0 1 2.824 -2.995l.176 -.005h2a1 1 0 0 1 1 1z" />
+    </g>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
+    <path d="M3 10h18" />
+    <path d="M10 3v18" />
   </svg>
 );
