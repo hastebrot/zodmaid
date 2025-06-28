c218b3415b1e54043959f51b5ab7bbe546dd8245
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-28 19:11:34 +0200

    [#20] feat: Data grid with columns, rows, cells, and group rows. (#19)

---
+++ packages/zodmaid-app/src/routes/grid/gridViewPage.tsx
@@ -0,0 +1,171 @@
+import { GridView, useGridView, type DataProps } from "../../components/grid/gridView";
+
+export const GridViewPage = () => {
+  return (
+    <div className="min-h-dvh bg-gray-100 text-gray-900 p-4 ">
+      <GridViewForOrder />
+    </div>
+  );
+};
+
+const GridViewForOrder = () => {
+  type DataModel = {
+    key: string;
+    value: unknown;
+  };
+  const data: DataProps<DataModel> = {
+    rows: [
+      { key: "orderDate", value: "2019-12-01" },
+      { key: "items", value: [] },
+      { key: "shipTo", value: {} },
+    ],
+  };
+  const gridView = useGridView({
+    data,
+    columns: [
+      {
+        key: "key",
+        label: "Key",
+        // width: "140px",
+      },
+      {
+        key: "value",
+        label: "Value",
+        cellRenderer(props) {
+          if (props.row.key === "items") {
+            return (
+              <div className="grid -mx-2">
+                <div className="grid -m-px overflow-hidden">
+                  <GridViewForItems columnOffset={props.columnIndex} rowOffset={props.rowIndex} />
+                </div>
+              </div>
+            );
+          }
+          if (props.row.key === "shipTo") {
+            return (
+              <div className="grid -mx-2">
+                <div className="grid -m-px overflow-hidden">
+                  <GridViewForShipTo columnOffset={props.columnIndex} rowOffset={props.rowIndex} />
+                </div>
+              </div>
+            );
+          }
+          return <div>{String(props.row.value) ?? "<empty>"}</div>;
+        },
+      },
+    ],
+    showRowLabels: true,
+    showColumnLabels: true,
+  });
+  return (
+    <div className="min-h-dvh bg-gray-100 text-gray-900 p-4">
+      <GridView value={gridView} />
+    </div>
+  );
+};
+
+const GridViewForItems = (props: { columnOffset?: number; rowOffset?: number }) => {
+  type DataModel = {
+    id: number;
+    productName: string;
+    quantity: number;
+    price: number;
+  };
+  const data: DataProps<DataModel> = {
+    rows: [
+      { id: 1, productName: "Diamond heart", quantity: 1, price: 10.95 },
+      { id: 2, productName: "Amber ring", quantity: 2, price: 20.95 },
+      { id: 3, productName: "Pearl necklace", quantity: 3, price: 30.95 },
+      { id: 4, productName: "Jade earring", quantity: 4, price: 40.95 },
+      { id: 5, productName: "Ruby bracelet", quantity: 5, price: 50.95 },
+    ],
+  };
+  const gridView = useGridView({
+    data,
+    columnsSpec: "auto max-content max-content 1fr",
+    columns: [
+      // { key: "id", label: "Id" },
+      {
+        key: "productName",
+        label: "Product Name",
+        // width: "140px",
+        // width: "max-content",
+        cellRenderer(props) {
+          return <div>{props.row.productName}</div>;
+        },
+        headerCellRenderer(props) {
+          return <div className="font-[700]">{props.label}</div>;
+        },
+      },
+      {
+        key: "quantity",
+        label: "Quantity",
+        // width: "max-content",
+        headerCellRenderer(props) {
+          return <div className="font-[700]">{props.label}</div>;
+        },
+      },
+      {
+        key: "price",
+        label: "Price",
+        // width: "100%",
+        headerCellRenderer(props) {
+          return <div className="font-[700]">{props.label}</div>;
+        },
+      },
+    ],
+    showRowLabels: false,
+    showColumnLabels: true,
+    columnOffset: props.columnOffset,
+    rowOffset: props.rowOffset,
+    fullWidth: true,
+  });
+  return <GridView value={gridView} />;
+};
+
+const GridViewForShipTo = (props: { columnOffset?: number; rowOffset?: number }) => {
+  type DataModel = {
+    key: string;
+    value: unknown;
+  };
+  const data: DataProps<DataModel> = {
+    rows: [
+      { key: "name", value: "Helen Zoe" },
+      { key: "street", value: "47 Eden Street" },
+      { key: "city", value: "Cambridge" },
+      { key: "postcode", value: "126" },
+    ],
+  };
+  const gridView = useGridView({
+    data,
+    columnsSpec: "auto max-content 1fr",
+    columns: [
+      {
+        key: "key",
+        label: "Key",
+        cellRenderer(props) {
+          return <div>{props.row.key}</div>;
+        },
+        headerCellRenderer(props) {
+          return <div className="font-[700]">{props.label}</div>;
+        },
+      },
+      {
+        key: "value",
+        label: "Value",
+        cellRenderer(props) {
+          return <div>{String(props.row.value)}</div>;
+        },
+        headerCellRenderer(props) {
+          return <div className="font-[700]">{props.label}</div>;
+        },
+      },
+    ],
+    showRowLabels: false,
+    showColumnLabels: true,
+    columnOffset: props.columnOffset,
+    rowOffset: props.rowOffset,
+    fullWidth: true,
+  });
+  return <GridView value={gridView} />;
+};
---
+++ packages/zodmaid-app/src/routes/grid/reactDataGridPage.tsx
@@ -0,0 +1,234 @@
+import { useEffect, useRef, useState, type Key } from "react";
+import { Cell, DataGrid, Row, textEditor, type Column, type DataGridHandle } from "react-data-grid";
+import "react-data-grid/lib/styles.css";
+import { classNames } from "../../helpers/clsx";
+
+export const ReactDataGridPage = () => {
+  const ref = useRef<DataGridHandle>(null);
+  const [rows, setRows] = useState<Row[]>(_rows);
+  const [selectedRows, setSelectedRows] = useState<Set<Key>>();
+  useEffect(() => {
+    console.log("selected row change");
+    console.log(selectedRows);
+  }, [selectedRows]);
+
+  useEffect(() => {
+    if (ref.current) {
+      // ref.current.element?.blur();
+      // ref.current.selectCell(
+      //   {
+      //     idx: -1,
+      //     rowIdx: -1,
+      //   },
+      //   { enableEditor: false },
+      // );
+    }
+  }, [ref]);
+  const onClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    if (ref.current) {
+      // ref.current.selectCell({ idx: 0, rowIdx: 0 }, { shouldFocusCell: true, enableEditor: false });
+    }
+  };
+
+  return (
+    <div className="min-h-dvh bg-[#212121] p-4">
+      <DataGrid
+        selectedRows={selectedRows}
+        onSelectedRowsChange={setSelectedRows}
+        className="!border-none !bg-[#212121] select-none"
+        ref={ref}
+        columns={columns}
+        rows={rows}
+        onRowsChange={setRows}
+        // headerRowHeight={0}
+        defaultColumnOptions={{
+          resizable: true,
+        }}
+        headerRowClass={classNames(
+          "[&>div]:border-t [&>div]:border-[#444444] [&>div]:first:border-l",
+        )}
+        renderers={{
+          renderCell(key, props) {
+            return <Cell key={key} {...props} className="border-[#444444] first:border-l" />;
+          },
+          renderRow(key, props) {
+            return <Row key={key} {...props} />;
+          },
+        }}
+        onSelectedCellChange={(event) => {
+          console.log("selected cell change", event);
+        }}
+        onCellMouseDown={(props, event) => {
+          if (event.target instanceof HTMLElement && event.target.tagName === "BUTTON") {
+            event.preventGridDefault();
+          }
+          // event.stopPropagation();
+        }}
+        onCellClick={(props, event) => {
+          event.preventGridDefault();
+          // console.log("cell click");
+          // event.stopPropagation();
+          // props.selectCell(true);
+        }}
+        onCellDoubleClick={(props, event) => {
+          event.preventGridDefault();
+        }}
+      />
+    </div>
+  );
+};
+
+interface Row {
+  index: number;
+  businessCase: string;
+  objectKey: string;
+  amount: number;
+  currency: string;
+  bookingDate: string;
+  paymentDate: string;
+  debtorId: string;
+  creditorId: string;
+  documentId: string;
+  parentId: string;
+  userId: string;
+  sourceSystem: string;
+  tariffCode: string;
+  agentId: string;
+  provisionAmount: number;
+  agioAmount: number;
+}
+
+const columns: Column<Row>[] = [
+  {
+    key: "index",
+    name: "Index",
+    width: "max-content",
+    renderCell(props) {
+      return <div className="text-gray-500">{props.row.index}</div>;
+    },
+  },
+  {
+    key: "businessCase",
+    name: "Business Case",
+    renderHeaderCell(props) {
+      return (
+        <div className="flex gap-1 items-center">
+          <div className="text-red-500 font-mono text-sm/none">{`{}`}</div>
+          <div>{props.column.name}</div>
+        </div>
+      );
+    },
+    renderCell(props) {
+      return (
+        <div className="flex gap-1 items-stretch">
+          <button
+            className="hover:outline cursor-pointer px-0.5 flex items-center"
+            type="button"
+            onClick={() => {
+              console.log("button");
+            }}
+          >
+            <div className="text-blue-500 font-mono text-sm/none">{`str`}</div>
+          </button>
+          <div className="flex gap-1 items-center">
+            <div className="text-green-500 font-mono text-sm/none">{`//`}</div>
+            <div className="text-red-500 font-mono text-sm/none">{`<>`}</div>
+            <div className="text-red-500 font-mono text-sm/none">{`{}`}</div>
+            <div className="text-red-500 font-mono text-sm/none">{`[]`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`str`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`num`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`bool`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`null`}</div>
+            <div className="text-blue-500 font-mono text-sm/none">{`f(x)`}</div>
+            {props.row.businessCase}
+          </div>
+        </div>
+      );
+    },
+    renderEditCell: textEditor,
+    editable: true,
+  },
+  {
+    key: "objectKey",
+    name: "Object Key",
+  },
+  {
+    key: "amount",
+    name: "Amount",
+  },
+  {
+    key: "currency",
+    name: "Currency",
+  },
+  {
+    key: "bookingDate",
+    name: "Booking Date",
+  },
+  {
+    key: "paymentDate",
+    name: "Payment Date",
+  },
+  {
+    key: "debtorId",
+    name: "Debtor ID",
+  },
+  {
+    key: "creditorId",
+    name: "Creditor ID",
+  },
+  {
+    key: "documentId",
+    name: "Document ID",
+  },
+  {
+    key: "parentId",
+    name: "Parent ID",
+  },
+  {
+    key: "userId",
+    name: "User ID",
+  },
+  {
+    key: "sourceSystem",
+    name: "Source System",
+  },
+  {
+    key: "tariffCode",
+    name: "Tariff Code",
+  },
+  {
+    key: "agentId",
+    name: "Agent ID",
+  },
+  {
+    key: "provisionAmount",
+    name: "Provision Amount",
+  },
+  {
+    key: "agioAmount",
+    name: "Agio Amount",
+  },
+];
+
+const _rows: Row[] = [
+  {
+    index: 1,
+    businessCase: "contract_payment",
+    objectKey: "CONTAINER_123456",
+    amount: 1250.0,
+    currency: "EUR",
+    bookingDate: "2025-06-24",
+    paymentDate: "2025-06-24",
+    debtorId: "CRM_USER_00123",
+    creditorId: "ASSET_MGR_00987",
+    documentId: "INV_2025_0633",
+    parentId: "CHAIN_473928",
+    userId: "SYS_FINANCEBOT",
+    sourceSystem: "CRM",
+    tariffCode: "T-STD-24A",
+    agentId: "AGT_333",
+    provisionAmount: 50.0,
+    agioAmount: 10.0,
+  },
+];
